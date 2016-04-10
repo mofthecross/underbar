4d5bc63a4c5f7b242bf9190d65cf4645495a2a2b
mofthecross <mofthecross@gmail.com>
2016-04-10 13:43:32 -0700

    Finish implementing identity, last, reject and filter functions.

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+  	return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+  	return n === undefined ? array[array.length - 1] : array.slice(Math.max(0, array.length - n) );
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -75,12 +77,23 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(item) {
+      if (test(item) === true) {
+        result.push(item);
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item){
+      return !test(item);
+    })
   };
 
   // Produce a duplicate-free version of the array.
